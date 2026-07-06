e0e86762e79a5758c76f9ef5f19ccafc5ae33f87
Mei Nagai <meisuma@icloud.com>
2026-07-06 17:08:01 +0900

    feat: initialize Spring Boot backend

---
+++ backend/designme-api/src/main/java/com/designme/designme_api/DesignmeApiApplication.java
@@ -0,0 +1,13 @@
+package com.designme.designme_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignmeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignmeApiApplication.class, args);
+	}
+
+}
---
+++ backend/designme-api/src/test/java/com/designme/designme_api/DesignmeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designme.designme_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignmeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
